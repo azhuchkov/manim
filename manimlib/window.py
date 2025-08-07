7e962777dadd02bae40af3ff3f8f494a932938b1
Andrey Zhuchkov <765471+azhuchkov@users.noreply.github.com>
2025-08-08 00:54:36 +0400

    Add closing state and destroy handling to pyglet window

--- manimlib/window.py
+++ manimlib/window.py
@@ -44,6 +44,7 @@ class Window(PygletWindow):
         self.default_position = position or self.position_from_string(position_string)
         self.pressed_keys = set()
         self._has_undrawn_event = True
+        self._is_closing = False
 
         config = pyglet.gl.Config(
             sample_buffers=1 if samples > 0 else 0,
@@ -274,9 +275,20 @@ class Window(PygletWindow):
     @note_undrawn_event
     def on_close(self) -> None:
         super().on_close()
+        self._is_closing = True
         if not self.scene:
             return
         self.scene.on_close()
 
     def is_key_pressed(self, symbol: int) -> bool:
         return (symbol in self.pressed_keys)
+
+    # Methods for compatibility with previous window wrapper
+    @property
+    def is_closing(self) -> bool:
+        return self._is_closing
+
+    def destroy(self) -> None:
+        if hasattr(self, "ctx"):
+            self.ctx.release()
+        self.close()
